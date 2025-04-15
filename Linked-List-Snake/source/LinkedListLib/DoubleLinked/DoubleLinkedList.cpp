5855a0eedc2c24978fbf8ec7d4f8edbfbe08ea22
pranaysj <pranaysj3@gmail.com>
2025-04-15 16:36:54 +0530

    Splitting in Half - DLL
    IMPLEMENTING remove half nodes

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -221,6 +221,28 @@ namespace LinkedListLib
 			}
 		}
 
+		void DoubleLinkedList::removeHalfNodes()
+		{
+			if (linked_list_size <= 1) return;
+
+			int half_length = linked_list_size / 2;
+			int new_tail_index = half_length - 1;
+
+			Node* prev_node = findNodeAtIndex(new_tail_index);
+			Node* cur_node = prev_node->next;
+
+			while (cur_node != nullptr)
+			{
+				Node* node_to_delete = cur_node;
+				cur_node = cur_node->next;
+
+				delete (node_to_delete);
+				linked_list_size--;
+			}
+
+			prev_node->next = nullptr;
+		}
+
 		void DoubleLinkedList::shiftNodesAfterRemoval(Node* cur_node)
 		{
 			sf::Vector2i previous_node_position = cur_node->bodypart.getPosition();
