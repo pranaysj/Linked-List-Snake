0b7cd61e651ea434ac90ccf8cc9d5bf04eadfec0
pranaysj <pranaysj3@gmail.com>
2025-04-15 15:52:56 +0530

    Insertion at the Middle - DLL
    Implementing SHIFT NODES AFTER insertion
    Implementing insert NODE AT INDEX
    Implementing insert NODE AT MIDDLE

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -35,8 +35,95 @@ namespace LinkedListLib
 			head_node = new_node;
 		}
 
+		void DoubleLinkedList::insertNodeAtMiddle()
+		{
+			if (head_node == nullptr) {
+				insertNodeAtHead();             // If the list is empty, insert at the head.
+				return;
+			}
+
+			int midIndex = findMiddleNode();
+			insertNodeAtIndex(midIndex);
+		}
+
 		void DoubleLinkedList::insertNodeAtTail()
 		{
+			linked_list_size++;
+			Node* new_node = createNode();
+			Node* cur_node = head_node;
+
+			if (cur_node == nullptr)
+			{
+				head_node = new_node;
+				static_cast<DoubleNode*>(new_node)->previous = nullptr;
+				initializeNode(new_node, nullptr, Operation::TAIL);
+				return;
+			}
+			while (cur_node->next != nullptr)
+			{
+				cur_node = cur_node->next;
+			}
+
+			cur_node->next = new_node;
+			static_cast<DoubleNode*>(new_node)->previous = cur_node;
+			initializeNode(new_node, cur_node, Operation::TAIL);
+		}
+
+		void DoubleLinkedList::insertNodeAtIndex(int index)
+		{
+			if (index < 0 || index >= linked_list_size) return;
+
+			if (index == 0)
+			{
+				insertNodeAtHead();
+				return;
+			}
+			Node* new_node = createNode();
+
+			int current_index = 0;
+			Node* cur_node = head_node;
+			Node* prev_node = nullptr;
+
+			while (cur_node != nullptr && current_index < index)
+			{
+				prev_node = cur_node;
+				cur_node = cur_node->next;
+				current_index++;
+			}
+
+			prev_node->next = new_node;
+			static_cast<DoubleNode*>(new_node)->previous = prev_node;
+			new_node->next = cur_node;
+			static_cast<DoubleNode*>(cur_node)->previous = new_node;
+
+			initializeNode(new_node, prev_node, Operation::TAIL); 
+			linked_list_size++;
+
+			shiftNodesAfterInsertion(new_node, cur_node, prev_node);
+		}
+
+		void DoubleLinkedList::shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node)
+		{
+			Node* next_node = cur_node;
+			cur_node = new_node;
+
+			while (cur_node != nullptr && next_node != nullptr)
+			{
+				cur_node->bodypart.setPosition(next_node->bodypart.getPosition());
+				cur_node->bodypart.setDirection(next_node->bodypart.getDirection());
+
+				prev_node = cur_node;
+				cur_node = next_node;
+				next_node = next_node->next;
+			}
+
+			initializeNode(cur_node, prev_node, Operation::TAIL);
+
+		}
+
+		void DoubleLinkedList::removeNodeAtHead()
+		{
+
 		}
 
 	}
